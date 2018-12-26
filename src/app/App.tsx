ea72e7bb413ac7ef80296058fe473f6916c52bbd
Marc MICHIELI <zyx1@free.fr>
2018-12-26 01:58:23 +0100

    Initial Commit

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+/**
+ * Root Component for Justine & Co
+ */
+export default class App extends React.Component {
+  public render() {
+    return <div>Justine&CO !!!</div>;
+  }
+}
